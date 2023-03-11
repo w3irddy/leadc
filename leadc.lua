73b54986872b1560140f562ab64d1031a442d9a3
w3irddy <118191333+w3irddy@users.noreply.github.com>
2023-03-11 16:15:08 +0300

    Update leadc.lua

--- leadc.lua
+++ leadc.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/leadc/main/main.lua", true))();
+local a,b = pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/leadc/main/games/"..game.PlaceId..".lua", true))() end)
+if a then
+    warn("supported")
+elseif b then
+    warn("not supported, executing universal")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/leadc/main/main.lua", true))()
+end
